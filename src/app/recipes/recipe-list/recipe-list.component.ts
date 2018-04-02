120c9606b62ebd43907bd7f8933919f235d4cfcf
dino buljubasic <dbnex14@gmail.com>
2018-04-02 13:44:15 -0700

    Added recipe list to show 2 hardoced recipes in the list all dynamically bound to array of recipes.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe'
+      , 'This is a simple test recipe'
+      , 'https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_1280.jpg'),
+      new Recipe('A Test Recipe 2'
+      , 'This is a simple test recipe 2'
+      , 'https://pbs.twimg.com/profile_images/750559002166263808/dVrxkvEM_400x400.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
